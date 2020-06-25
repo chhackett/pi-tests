4073f3122ea98580f8c368e5cb14c29433d4c27a
Chris Hackett <chris.h.hackett@gmail.com>
2020-06-25 10:10:50 -0400

    Trying to make code more efficient

--- TrieNode.hs
+++ TrieNode.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module TrieNode where
 
 import Data.Maybe
@@ -11,14 +13,14 @@ empty = TrieNode Nothing []
 getKeys = map fst
 
 insert :: Eq a => String -> a -> TrieNode a -> TrieNode a
-insert [] _ node  = node
-insert [c] v node =
-    let kids = children node; val = value node
+insert [] _ !node  = node
+insert [c] v !node =
+    let !kids = children node; val = value node
     in if c `elem` getKeys kids
-       then TrieNode val $ replaceNode c v kids
-       else TrieNode val ((c, TrieNode (Just v) []):kids)
-insert (c:cs) v node =
-    let kids = children node; val = value node
+       then TrieNode val $! replaceNode c v kids
+       else TrieNode val $! ((c, TrieNode (Just v) []):kids)
+insert (c:cs) v !node =
+    let !kids = children node; val = value node
     in if c `elem` getKeys kids
        then TrieNode val (map (\kid -> if fst kid == c
                                        then (c, insert cs v (snd kid))
@@ -32,18 +34,18 @@ replaceNode c val =
     map (\node -> if fst node == c
                   then (c, TrieNode (put val $ snd node) (children $ snd node))
                   else node)
-    where put val' node' =
+    where put val' !node' =
            if isNothing (value node')
            then Just val'
            else value node'
 
 get :: Eq a => String -> TrieNode a -> Maybe a
 get [] _     = Nothing
-get [c] t    = value =<< getValue c (children t)
-get (c:cs) t = case getValue c (children t) of
+get [c] !t    = value =<< getValue c (children t)
+get (c:cs) !t = case getValue c (children t) of
                      Nothing -> Nothing
                      Just t' -> get cs t'
 
 getValue :: Char -> [(Char, TrieNode a)] -> Maybe (TrieNode a)
-getValue c ts = let ts' = filter (\t -> fst t == c) ts
+getValue c !ts = let ts' = filter (\t -> fst t == c) ts
                 in if null ts' then Nothing else Just (snd $ head ts')
